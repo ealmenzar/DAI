d21159b3a7cf7b5143f9048d7972bdb9ef9be128
Marina E. Almenzar <Marina E. Almenzar>
2016-12-02 02:13:29 +0100

    Añadidos comienzos con MongoDB

--- web/web.py
+++ web/web.py
@@ -1,12 +1,30 @@
 from flask import Flask
 from flask import render_template, request, session, url_for, redirect
 import shelve
+import pymongo
 app = Flask(__name__)
 
 app.debug = True
 #usuarios = {}
 data = shelve.open('data', writeback=True)
 s = []
+
+# Connection to Mongo DB
+try:
+    conn = pymongo.MongoClient()
+    print("Connected successfully!!!")
+except pymongo.errors.ConnectionFailure as e:
+    print("Could not connect to MongoDB: %s" % e)
+
+#print(conn)
+
+db = conn.test
+#restaurants = db.primer_dataset
+cursor = db.restaurants.find()
+
+#for doc in cursor:
+ #   print(doc)
+
 
 @app.route('/')
 def index():
@@ -113,6 +131,83 @@ def profile():
                                lastname=data[u]['lastname'], lastlastname=data[u]['lastlastname'])
     else:
         return render_template('formulario.html')
+
+@app.route('/Restaurantes')
+def restaurants():
+    if 'username' in session:
+        s = session['paginas']
+        s.insert(0, 'Restaurantes')
+        session['paginas'] = s
+        if len(s) == 4:
+            s.remove(s[3])
+
+        return render_template('restaurantes.html', usuario=session['username'], pags=session['paginas'])
+    return render_template('formulario.html')
+
+@app.route('/AgregarRestaurante', methods=['GET', 'POST'])
+def addrestaurant():
+    if 'username' in session:
+        s = session['paginas']
+        s.insert(0, 'AgregarRestaurante')
+        session['paginas'] = s
+        if len(s) == 4:
+            s.remove(s[3])
+
+        if request.method == 'POST':
+            building = int(request.form['building'])
+            coord1 = float(request.form['coord1'])
+            coord2 = float(request.form['coord2'])
+            street = str(request.form['street'])
+            zipcode = int(request.form['zipcode'])
+            borough = str(request.form['borough'])
+            cuisine = str(request.form['cuisine'])
+            name = str(request.form['name'])
+            id = int(request.form['id'])
+
+            dic = {"address":
+                       {"building": building,
+                        "coord": [coord1, coord2],
+                        "street": street,
+                        "zipcode": zipcode},
+                   "borough": borough,
+                   "cuisine": cuisine,
+                   "grades": [
+                   #    {"date": {"$date": 1393804800000}, "grade": "A", "score": 2},
+                   #    {"date": {"$date": 1378857600000}, "grade": "A", "score": 6},
+                   #    {"date": {"$date": 1358985600000}, "grade": "A", "score": 10},
+                   #    {"date": {"$date": 1322006400000}, "grade": "A", "score": 9},
+                   #    {"date": {"$date": 1299715200000}, "grade": "B", "score": 14}
+                   ],
+                   "name": name,
+                   "restaurant_id": id
+                   }
+            restaurants.insert(dic)
+            return render_template('agregarrestaurante.html', usuario=session['username'], pags=session['paginas'])
+        return render_template('agregarrestaurante.html', usuario=session['username'], pags=session['paginas'])
+    else:
+        return render_template('formulario.html')
+
+@app.route("/BuscarRestaurante", methods=['GET', 'POST'])
+def lookforrest():
+    if 'username' in session:
+        s = session['paginas']
+        s.insert(0, 'BuscarRestaurante')
+        session['paginas'] = s
+        if len(s) == 4:
+            s.remove(s[3])
+
+        if request.method == 'POST':
+            name = str(request.form['name'])
+
+            dic = {"name": name}
+            R = restaurants.find(dic)
+            print(R)
+            return render_template('buscarrestaurante.html', usuario=session['username'],
+                                   pags=session['paginas'], restaurante=R)
+        return render_template('buscarrestaurante.html', usuario=session['username'], pags=session['paginas'])
+    else:
+        return render_template('formulario.html')
+
 
 
 @app.route('/logout')
